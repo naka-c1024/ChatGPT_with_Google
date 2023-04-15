f865f8d2e818dcbe01aa4af2d1121dcf4ad4cdb4
naka-c1024 <ny42800824@gmail.com>
2023-04-15 19:01:15 +0900

    add: 検索機能を追加

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 from flask import Flask, jsonify, request, render_template
 from dotenv import load_dotenv
 import openai
+from langchain.llms import OpenAI
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
 
 load_dotenv()
 # Make sure API key is set
-if not os.environ.get("OPENAI_API_KEY"):
-    raise RuntimeError("OPENAI_API_KEY not set")
+if not os.environ["OPENAI_API_KEY"] or not os.environ["GOOGLE_API_KEY"] or not os.environ["GOOGLE_CSE_ID"]:
+    raise RuntimeError("API key not set")
 
 app = Flask(__name__)
 
@@ -15,7 +18,9 @@ app = Flask(__name__)
 def index():
     # POST メソッドでアクセスされた場合
     if request.method == "POST":
-        # AIに送るメッセージ
+
+        # openaiをそのままつかう場合
+        '''
         user_msg = request.form["user_msg"]
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -27,6 +32,28 @@ def index():
             ],
         )
         res_msg = response.choices[0]["message"]["content"]
+        '''
+
+        # langchainをつかう場合
+        '''
+        #プロンプト
+        prompt = request.form["user_msg"]
+        #LLMの設定
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=os.environ["OPENAI_API_KEY"])
+        #推論を実行
+        res_msg = llm(prompt)
+        '''
+
+        # Google検索をつかう場合(https://murasan-net.com/index.php/2023/04/03/chatgpt-langchain-web-newinfo/)
+        # LLMを設定
+        llm = OpenAI(model_name="gpt-3.5-turbo")
+        # ツールを設定
+        tools = load_tools(["google-search"], llm=llm)
+        # エージェントの作成
+        agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
+        # 推論を実行
+        res_msg = agent.run(request.form["user_msg"])
+
         return render_template("index.html", res_msg=res_msg)
 
     # GET メソッドでアクセスされた場合
