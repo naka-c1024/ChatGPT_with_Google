4ef7d9926e843b721c72b2dd9e1db9d45b14c7c2
naka-c1024 <ny42800824@gmail.com>
2023-04-16 10:07:16 +0900

    clean: リファクタリング

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-ChatGPTを使用したアプリを作成してみる。
+# ChatGPT with Google
+
+ChatGPTにGoogle検索機能をつけてレスポンスしてくれるWebアプリです。
+
+# Features
+
+現在のChatGPTは学習しているデータが2021年末までのものであり、最新情報に回答してくれません。
+そのためLangChainとGoogle APIを使用して、Web検索を元に回答してくれるアプリを作成しました。
+
+# Usage
+
+## Git clone
+
+```
+git clone https://github.com/naka-c1024/ChatGPT_with_Google.git
+```
+
+## Preparation
+
+### Dockerを使用する場合
+
+```bash
+docker build -t gpt_app .
+docker run -v $(pwd):/app -p 8080:8080 -it gpt_app bash
+```
+
+### Dockerを使用せずローカル環境で実行したい場合
+
+#### Requirement
+
+- Flask 2.1.0
+- python-dotenv 1.0.0
+- openai 0.27.4
+- langchain 0.0.139
+- google-api-python-client 2.85.0
+
+#### Installation
+
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+```bash
+python main.py
+```
+
+# 使用技術
+
+### 環境: Docker
+
+### フロントエンド: HTML，CSS (TailWindCSS)
+
+### バックエンド: Python (Flask)
+
+### API: Google Programmable Search Engine API, OpenAI API
--- main.py
+++ main.py
@@ -1,64 +1,75 @@
 import os
 
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 from dotenv import load_dotenv
-import openai
-from langchain.llms import OpenAI
-from langchain.agents import load_tools
-from langchain.agents import initialize_agent
+from langchain.agents import ZeroShotAgent, AgentExecutor, load_tools
+from langchain import OpenAI, LLMChain
 
+# 環境変数を読み込む
 load_dotenv()
-# Make sure API key is set
+
+# APIキーが設定されていることを確認
 if not os.environ["OPENAI_API_KEY"] or not os.environ["GOOGLE_API_KEY"] or not os.environ["GOOGLE_CSE_ID"]:
-    raise RuntimeError("API key not set")
+    raise RuntimeError("APIキーが設定されていません")
 
 app = Flask(__name__)
 
+def create_agent_executor():
+    """
+    エージェント実行者のインスタンスを作成して、ユーザー入力を処理します。
+    """
+    llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    tools = load_tools(["google-search"], llm=llm)
+    prompt = create_agent_prompt(tools)
+
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools)
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+
+    return agent_executor
+
+def create_agent_prompt(tools):
+    """
+    ZeroShotAgent用のエージェントプロンプトを作成します。
+    """
+    prefix = """次の質問にできる限り答えてください。次のツールにアクセスできます:"""
+    suffix = """最終的な答えを出すときは、日本語で出力してください。
+
+    Question: {input}
+    {agent_scratchpad}
+    """
+
+    return ZeroShotAgent.create_prompt(
+        tools,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["input", "agent_scratchpad"]
+    )
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # POST メソッドでアクセスされた場合
     if request.method == "POST":
+        # ユーザー入力を処理するエージェント実行者を作成
+        agent_executor = create_agent_executor()
 
-        # openaiをそのままつかう場合
-        '''
-        user_msg = request.form["user_msg"]
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                # {"role": "system", "content": "ここで役割を指定する。"},
-                # {"role": "user", "content": '初メッセージ'},
-                # {"role": "assistant", "content": 'レスポンスを入れる'},
-                {"role": "user", "content": user_msg},
-            ],
-        )
-        res_msg = response.choices[0]["message"]["content"]
-        '''
-
-        # langchainをつかう場合
-        '''
-        #プロンプト
-        prompt = request.form["user_msg"]
-        #LLMの設定
-        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=os.environ["OPENAI_API_KEY"])
-        #推論を実行
-        res_msg = llm(prompt)
-        '''
-
-        # Google検索をつかう場合(https://murasan-net.com/index.php/2023/04/03/chatgpt-langchain-web-newinfo/)
-        # LLMを設定
-        llm = OpenAI(model_name="gpt-3.5-turbo")
-        # ツールを設定
-        tools = load_tools(["google-search"], llm=llm)
-        # エージェントの作成
-        agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=False)
-        # 推論を実行
-        res_msg = agent.run(request.form["user_msg"])
+        # エージェントの実行中に発生する例外を処理
+        try:
+            res_msg = agent_executor.run(request.form["user_msg"])
+        except ValueError as e:
+            res_msg = f"エラーが発生しました: {str(e)}"
+        except Exception as e:
+            res_msg = f"予期しないエラーが発生しました: {str(e)}"
 
         return render_template("index.html", res_msg=res_msg)
 
-    # GET メソッドでアクセスされた場合
+    # GETリクエストの場合、メインページを表示
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+'''
+参考
+https://murasan-net.com/index.php/2023/04/03/chatgpt-langchain-web-newinfo/
+https://note.com/npaka/n/nd9a4a26a8932
+'''
